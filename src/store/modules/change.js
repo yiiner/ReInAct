249b54311d3598475f04f8ea96a065a7bf2a4616
EMT <emilia_subaru@163.com>
2024-09-05 20:31:31 +0800

    add page change logic in change.js (not use)

---
+++ src/store/modules/change.js
@@ -0,0 +1,35 @@
+export default {
+    namespaced: true,
+
+    state() {
+        return {
+            // change the content page show in app
+            showPage: true,
+
+            // determine export mode
+            exportDataReady: false,
+        };
+    },
+
+    getters: {
+        showPage(state) {
+            return state.showPage;
+        },
+
+        exportDataReady(state) {
+            return state.exportDataReady;
+        },
+    },
+
+    mutations: {
+        setShowPage(store, payload) {
+            store.showPage = payload;
+        },
+
+        setExportDataReady(store, payload) {
+            store.setExportDataReady = payload;
+        },
+    },
+
+    actions: {},
+};
